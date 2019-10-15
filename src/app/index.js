c3fda1118ef55e6878891ecd33c7826bbf2c7f50
vivek_mt0399 <vivek.gopalan@myntra.com>
2019-10-15 13:12:39 +0530

    initial commit

---
+++ src/app/index.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+
+export default class DashBoard extends Component{
+
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+         
+        }
+       
+    }
+
+
+
+  
+
+
+
+    render(){
+        return (
+            <div>
+             Main Page
+             
+            
+            </div>
+        )
+    }
+
+}
