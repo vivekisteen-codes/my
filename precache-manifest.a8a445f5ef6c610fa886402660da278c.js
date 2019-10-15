efd0bbf7d811f5982b2a12a3aa57ba6b2d0d880b
vivek_mt0399 <vivek.gopalan@myntra.com>
2019-10-15 13:13:22 +0530

    Updates

---
+++ precache-manifest.a8a445f5ef6c610fa886402660da278c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24027ac1370fedc4a756bcda01fb8994",
+    "url": "/my/index.html"
+  },
+  {
+    "revision": "7acc4a0bfde95f145790",
+    "url": "/my/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "593f20d8ded79f6170a9",
+    "url": "/my/static/js/2.303e2f33.chunk.js"
+  },
+  {
+    "revision": "7acc4a0bfde95f145790",
+    "url": "/my/static/js/main.3c76c944.chunk.js"
+  },
+  {
+    "revision": "aa8473a08de8384e326b",
+    "url": "/my/static/js/runtime-main.c0b5e0e3.js"
+  }
+]);
